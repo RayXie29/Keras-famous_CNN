990ac9e53a95844ea80582a825a85c3c59d20a70
RayXie <rayxie0329@gmail.com>
2019-08-10 22:33:29 +0800

    modify InceptionV2

--- InceptionV2.py
+++ InceptionV2.py
@@ -1,23 +1,22 @@
 import keras
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-from keras.layers import Dense, BatchNormalization, Activation
-from keras.layers import Flatten, Input, concatenate, Dropout
-from keras.layers.merge import  add
+from keras.layers import Dense, Activation, BatchNormalization
+from keras.layers import Flatten, Input, Dropout, concatenate
 from keras.regularizers import  l2
 from keras import backend as K
 from keras.models import Model
-import numpy as np
 
 
 class Inceptionv2_builder():
 
-    def __init__(self, input_shape = (299,299,3), output_units = 1000, init_strides = (2,2),
+    def __init__(self, input_shape = (224,224,3), output_units = 1000, init_kernel = (7,7), init_strides = (2,2), init_filters = 64,
                  regularizer = l2(1e-4), initializer = "he_normal", init_maxpooling = True):
-
         '''
         :param input_shape: input shape of dataset
         :param output_units: output result dimension
+        :param init_kernel: The kernel size for first convolution layer
         :param init_strides: The strides for first convolution layer
+        :param init_filters: The filter number for first convolution layer
         :param regularizer: regularizer for all the convolution layers in whole NN
         :param initializer: weight/parameters initializer for all convolution & fc layers in whole NN
         :param init_maxpooling: Do the maxpooling after first two convolution layers or not
@@ -27,7 +26,9 @@ class Inceptionv2_builder():
 
         self.input_shape = input_shape
         self.output_units = output_units
+        self.init_kernel = init_kernel
         self.init_strides = init_strides
+        self.init_filters = init_filters
         self.regularizer = regularizer
         self.initializer = initializer
         self.init_maxpooling = init_maxpooling
@@ -44,22 +45,24 @@ class Inceptionv2_builder():
             self.col_axis = 3
             self.channel_axis = 1
 
-    def _cn_bn_relu(self, filters = 32, kernel_size = (3,3), strides = (1,1), padding = "same"):
+
+    def _cn_bn_relu(self, filters = 64, kernel_size = (3,3), strides = (1,1), padding = "same",
+                    kernel_regularizer = l2(1e-4), kernel_initializer = "he_normal"):
         '''
-        convenient function to build convolution -> batch_nromalization -> relu activation layers
+        convenient function to build convolution -> batch normalization -> relu activation layers
         '''
         def f(input_x):
 
-            x = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding,
-                       kernel_regularizer=self.regularizer,kernel_initializer=self.initializer)(input_x)
-            x = BatchNormalization(axis=self.channel_axis)(x)
+            x = Conv2D(filters = filters, kernel_size = kernel_size, strides = strides, padding = padding,
+                       kernel_initializer = kernel_initializer, kernel_regularizer = kernel_regularizer)(input_x)
+            x = BatchNormalization(axis = self.channel_axis)(x)
             x = Activation("relu")(x)
 
             return x
 
         return f
 
-    def _auxiliary(self):
+    def _auxiliary(self, name = "auxiliary_1"):
         '''
         In author's explanation:
 
@@ -70,133 +73,68 @@ class Inceptionv2_builder():
         '''
         def f(input_x):
 
-            x = AveragePooling2D(pool_size=(5,5), strides = (3,3), padding = "same")(input_x)
-            x = self._cn_bn_relu(filters = 128, kernel_size = (5,5), strides = (1,1), padding = "same")(x)
+            x = input_x
+            x = AveragePooling2D(pool_size=(5,5), strides = (3,3), padding = "same")(x)
+            x = self._cn_bn_relu(filters = 128, kernel_size = (1,1), padding = "same",
+                                 kernel_regularizer = self.regularizer, kernel_initializer = self.initializer)(x)
             x = Flatten()(x)
-            x = Dense(units = 1024, kernel_initializer = self.initializer)(x)
-            x = BatchNormalization(axis = self.channel_axis)(x)
+            x = Dense(units = 1024)(x)
+            x = BatchNormalization(axis = 1)(x)
             x = Activation("relu")(x)
             x = Dropout(0.7)(x)
 
-            output = Dense(units = self.output_units , activation = "softmax", kernel_initializer=self.initializer)(x)
-
-    def _inception_block35x35(self,_1x1 = 64, _3x3r = 48, _3x3 = 64, _d3x3r = 64, _d3x3 = 96, _pool = 64):
-        '''
-        A function for building inception block of figure5 in original article,
-        '''
-        def f(input_x):
-
-            branch1x1 = self._cn_bn_relu(filters = _1x1, kernel_size = (1,1))(input_x)
-
-            branchpooling = AveragePooling2D(pool_size=(3,3), strides = (1,1), padding = "same")(input_x)
-            branchpooling = self._cn_bn_relu(filters = _pool, kernel_size = (1,1))(branchpooling)
-
-            branch3x3 = self._cn_bn_relu(filters = _3x3r, kernel_size = (1,1))(input_x)
-            branch3x3 = self._cn_bn_relu(filters = _3x3, kernel_size = (3,3))(branch3x3)
-
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3r, kernel_size = (1,1))(input_x)
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3, kernel_size = (3,3))(dbranch3x3)
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3, kernel_size = (3,3))(dbranch3x3)
-
-            return concatenate([branch1x1, branchpooling, branch3x3, dbranch3x3], axis = self.channel_axis)
-
-        return f
-
-    def _GridSizeReduction35x35(self, _3x3r = 288, _3x3 = 384, _d3x3r = 64, _d3x3 = 96):
-        '''
-        A function for dimension reducing from 35x35 -> 17x17
-        '''
-        def f(input_x):
-
-            branchpool = AveragePooling2D(pool_size=(3,3), strides = (2,2), padding = "valid")(input_x)
-
-            branch3x3 = self._cn_bn_relu(filters = _3x3r, kernel_size = (1,1))(input_x)
-            branch3x3 = self._cn_bn_relu(filters = _3x3,  kernel_size = (3,3), strides = (2,2), padding = "valid")(branch3x3)
-
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3r, kernel_size = (1,1))(input_x)
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3,  kernel_size = (3,3))(dbranch3x3)
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3,  kernel_size = (3,3), strides = (2,2), padding = "valid")(dbranch3x3)
-
-            return concatenate([branchpool, branch3x3, dbranch3x3], axis = self.channel_axis)
-
-        return f
-
-    def _inception_block17x17(self, _1x1 = 192, _7x7r = 128, _7x7 = 192, _d7x7r = 128, _d7x7 = 192, _pool = 192):
-        '''
-        A function for building inception block of figure6 in original article,
-        '''
-        def f(input_x):
-
-            branch1x1 = self._cn_bn_relu(filters=_1x1, kernel_size=(1, 1))(input_x)
-
-            branchpooling = AveragePooling2D(pool_size = (3,3), strides = (1,1), padding = "same")(input_x)
-            branchpooling = self._cn_bn_relu(filters = _pool, kernel_size = (1,1))(branchpooling)
-
-            branch7x7 = self._cn_bn_relu(filters = _7x7r, kernel_size = (1,1))(input_x)
-            branch7x7 = self._cn_bn_relu(filters = _7x7r, kernel_size = (7,1))(branch7x7)
-            branch7x7 = self._cn_bn_relu(filters = _7x7, kernel_size=(1, 7))(branch7x7)
-
-            dbranch7x7 = self._cn_bn_relu(filters = _d7x7r, kernel_size = (1,1))(input_x)
-            for i in range(2):
-                dbranch7x7 = self._cn_bn_relu(filters = _d7x7r, kernel_size=(7, 1))(branch7x7)
-
-                if i == 0:
-                    dbranch7x7 = self._cn_bn_relu(filters=_d7x7r, kernel_size=(1, 7))(branch7x7)
-                else :
-                    dbranch7x7 = self._cn_bn_relu(filters=_d7x7, kernel_size=(1, 7))(branch7x7)
-
-
-            return concatenate([branch1x1, branchpooling, branch7x7, dbranch7x7], axis = self.channel_axis)
+            output = Dense(units = self.output_units, activation = "softmax", kernel_initializer = self.initializer, name = name)(x)
+            return output
 
         return f
 
 
-    def _GridSizeReduction17x17(self, _3x3r = 192, _3x3 =320, _d7x7x3r = 192, _d7x7x3 = 192):
+    def _inception_block(self, _1x1 = 64, _3x3r = 96, _3x3 = 128, _d3x3r = 16, _d3x3 = 32, _pool = 32, strides = (1,1), pooling = "avg"):
         '''
-        A function for dimension reducing from 17x17 -> 8x8
+        A function for building inception block, including 1x1 convolution layer, 3x3 convolution layer with dimension reducing,
+        double 3x3 convolution layers with dimension reducing and maxpooling layer with dimension reducing
+
+        :param _1x1: filter number of 1x1 convolution layer
+        :param _3x3r: filter number of dimension reducing layer for 3x3 convolution layer
+        :param _3x3: filter number of 3x3 convolution layer
+        :param _d3x3r: filter number of dimension reducing layer for double 3x3 convolution layers
+        :param _d3x3: filter number of double 3x3 convolution layers
+        :param _maxpool: filter number of dimension reducing layer for maxpooling layer
+        :return: A concatenate block of several scale convolution which is inception block
         '''
         def f(input_x):
 
-            branchpool = AveragePooling2D(pool_size = (3,3), strides = (2,2), padding = "valid")(input_x)
+            branch1x1 = None
+            if _1x1 > 0:
+                branch1x1 = self._cn_bn_relu(filters=_1x1, kernel_size=(1, 1), strides=(1, 1), padding="same",
+                                             kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(input_x)
 
-            branch7x7 = self._cn_bn_relu(filters = _3x3r, kernel_size = (1,1))(input_x)
-            branch7x7 = self._cn_bn_relu(filters = _3x3 , kernel_size = (3,3), strides = (2,2), padding = "valid")(branch7x7)
+            branch3x3 = self._cn_bn_relu(filters=_3x3r, kernel_size=(1, 1), strides=(1, 1), padding="same",
+                                         kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(input_x)
+            branch3x3 = self._cn_bn_relu(filters=_3x3, kernel_size=(3, 3), strides= strides, padding="same",
+                                         kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(branch3x3)
 
-            dbranch7x7 = self._cn_bn_relu(filters = _d7x7x3r, kernel_size = (1, 1))(input_x)
-            dbranch7x7 = self._cn_bn_relu(filters = _d7x7x3, kernel_size = (7, 1))(dbranch7x7)
-            dbranch7x7 = self._cn_bn_relu(filters = _d7x7x3, kernel_size = (1, 7))(dbranch7x7)
-            dbranch7x7 = self._cn_bn_relu(filters = _d7x7x3,  kernel_size = (3, 3), strides = (2, 2), padding = "valid")(dbranch7x7)
+            dbranch3x3 = self._cn_bn_relu(filters=_d3x3r, kernel_size=(1, 1), strides = (1, 1), padding="same",
+                                         kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(input_x)
+            dbranch3x3 = self._cn_bn_relu(filters=_d3x3, kernel_size=(3, 3), strides = (1, 1), padding="same",
+                                         kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(dbranch3x3)
+            dbranch3x3 = self._cn_bn_relu(filters=_d3x3, kernel_size=(3, 3), strides = strides, padding="same",
+                                          kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(dbranch3x3)
 
-            return concatenate([branchpool, branch7x7, dbranch7x7], axis = self.channel_axis)
+            brancemaxpool = None
+            if pooling == "avg":
+                brancemaxpool = AveragePooling2D(pool_size=(3, 3), strides=(1, 1), padding="same")(input_x)
+                brancemaxpool = self._cn_bn_relu(filters=_pool, kernel_size=(1, 1), strides=(1, 1), padding="same",
+                                                 kernel_regularizer=self.regularizer, kernel_initializer=self.initializer)(brancemaxpool)
+            else:
+                brancemaxpool = MaxPooling2D(pool_size=(3,3), strides = strides, padding = "same")(input_x)
 
-        return f
-
-    def _inception_block8x8(self, _1x1 = 320, _pool = 192, _3x3r = 384, _3x3 = 384, _d3x3r = 448, _d3x3 = 384):
-        '''
-        A function for building inception block of figure7 in original article,
-        '''
-
-        def f(input_x):
-
-            branch1x1 = self._cn_bn_relu(filters = _1x1, kernel_size = (1,1))(input_x)
-
-            branchpool = AveragePooling2D(pool_size = (3,3), strides = (1,1), padding = "same")(input_x)
-            branchpool = self._cn_bn_relu(filters = _pool, kernel_size = (1,1))(branchpool)
-
-            branch3x3 = self._cn_bn_relu(filters = _3x3r, kernel_size = (1,1))(input_x)
-            branch3x3_1 = self._cn_bn_relu(filters = _3x3, kernel_size = (3,1))(branch3x3)
-            branch3x3_2 = self._cn_bn_relu(filters = _3x3, kernel_size = (1,3))(branch3x3)
-
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3r, kernel_size = (1,1))(input_x)
-            dbranch3x3 = self._cn_bn_relu(filters = _d3x3, kernel_size = (3,3))(dbranch3x3)
-            dbranch3x3_1 = self._cn_bn_relu(filters = _d3x3, kernel_size = (3,1))(dbranch3x3)
-            dbranch3x3_2 = self._cn_bn_relu(filters = _d3x3, kernel_size = (1,3))(dbranch3x3)
-
-            return concatenate([branch1x1, branchpool, branch3x3_1, branch3x3_2, dbranch3x3_1, dbranch3x3_2], axis = self.channel_axis)
+            if _1x1 > 0:
+                return concatenate([branch1x1, branch3x3, dbranch3x3, brancemaxpool], axis=self.channel_axis)
+            else :
+                return concatenate([branch3x3, dbranch3x3, brancemaxpool], axis=self.channel_axis)
 
         return f
-
-
     def build_inception(self):
 
         '''
@@ -204,50 +142,71 @@ class Inceptionv2_builder():
         :return: An inceptionV2 nn
         '''
 
-        input_x = Input(self.input_shape)
-
-
         #Few traditional convolutional layers at lower layers
-        #Which are factorized by original 7x7 convolution layer
-        x = self._cn_bn_relu(filters = 32, kernel_size = (3,3), strides = self.init_strides, padding = "valid")(input_x)
-        x = self._cn_bn_relu(filters = 32, kernel_size = (3,3), strides = (1,1), padding = "valid")(x)
-        x = self._cn_bn_relu(filters = 64, kernel_size = (3,3), strides=(1,1), padding="same")(x)
+        input_x = Input(self.input_shape)
+        x = self._cn_bn_relu(filters = self.init_filters, kernel_size = self.init_kernel, strides = self.init_strides,
+                             kernel_regularizer = self.regularizer, kernel_initializer = self.initializer)(input_x)
 
         if self.init_maxpooling:
-            x = MaxPooling2D(pool_size = (3,3), strides = (2,2), padding = "valid")(x)
+            x = MaxPooling2D(pool_size = (3,3), strides = (2,2), padding = "same")(x)
 
-        x = self._cn_bn_relu(filters = 80, kernel_size = (3,3), strides=(1,1), padding = "valid")(x)
-        x = self._cn_bn_relu(filters = 192, kernel_size = (3,3), strides = self.init_strides, padding = "valid")(x)
-        x = self._cn_bn_relu(filters = 288, kernel_size = (3,3), strides = (1,1), padding = "same")(x)
+        x = self._cn_bn_relu(filters = 64, kernel_size = (1,1), strides = (1,1), padding = "same",
+                             kernel_regularizer = self.regularizer, kernel_initializer = self.initializer)(x)
+        x = self._cn_bn_relu(filters = 192, kernel_size = (3,3), strides = (1, 1), padding = "same",
+                             kernel_regularizer = self.regularizer, kernel_initializer = self.initializer)(x)
 
-        #First 3 inception block, which are using architecture of figure5 in original article
-        for i in range(3):
-            x = self._inception_block35x35(_1x1=64,_3x3r=48,_3x3=64,_d3x3r=64,_d3x3=96)(x)
+        if self.init_maxpooling:
+            x = MaxPooling2D(pool_size = (3,3), strides = (2,2), padding = "same")(x)
 
-        #Dimension reducing #1 (from 35x35 -> 17x17 in original article)
-        x = self._GridSizeReduction35x35(_3x3r=288, _3x3=184,_d3x3r=64,_d3x3=96)(x)
 
-        # 5 inception block, which are using architecture of figure6 in original article
-        for i in range(5):
-            x = self._inception_block17x17(_1x1=192,_7x7r=128,_7x7=192,_d7x7r=128,_d7x7=192,_pool=192)(x)
 
-        # auxiliary classifier
-        auxiliary = self._auxiliary()(x)
+        #inception(3a)
+        x = self._inception_block(_1x1=64, _3x3r=64, _3x3=64, _d3x3r=64, _d3x3=96, _pool=32)(x)
 
-        #Dimension reducing #2 (from 17x17 -> 8x8 in original article)
-        x = self._GridSizeReduction17x17(_3x3r=192,_3x3=320,_d7x7x3r=192,_d7x7x3=192)(x)
+        #inception(3b)
+        x = self._inception_block(_1x1=64, _3x3r=64, _3x3=96, _d3x3r=64, _d3x3=96, _pool=64)(x)
 
-        for i in range(2):
-            x = self._inception_block8x8(_1x1 = 320, _pool = 192, _3x3r = 384, _3x3 = 384, _d3x3r = 448, _d3x3 = 384)(x)
+        #inception(3c)
+        x = self._inception_block(_1x1=0, _3x3r=128, _3x3=160, _d3x3r=64, _d3x3=96,_pool=0,strides=(2,2),pooling="max")(x)
+
+        #inception(4a)
+        x = self._inception_block(_1x1=224, _3x3r=64, _3x3=96, _d3x3r=96, _d3x3=128, _pool=128)(x)
+
+        #auxiliary classifier 1
+        auxiliary1 = self._auxiliary(name = "auxiliary_1")(x)
+
+        # inception(4b)
+        x = self._inception_block(_1x1=192, _3x3r=96, _3x3=128, _d3x3r=96, _d3x3=128, _pool=128)(x)
+        # inception(4c)
+        x = self._inception_block(_1x1=160, _3x3r=128, _3x3=160, _d3x3r=128, _d3x3=160, _pool=128)(x)
+        # inception(4d)
+        x = self._inception_block(_1x1=96, _3x3r=128, _3x3=192, _d3x3r=160, _d3x3=192, _pool=128)(x)
+
+        #auxiliary classifier 2
+        auxiliary2 = self._auxiliary(name = "auxiliary_2")(x)
+
+        # inception(4e)
+        x = self._inception_block(_1x1=0, _3x3r=128, _3x3=192, _d3x3r=192, _d3x3=256, _pool=0, strides = (2,2), pooling = "max")(x)
+
+        #inception(5a)
+        x = self._inception_block(_1x1=352, _3x3r=192, _3x3=320, _d3x3r=160, _d3x3=224, _pool=128)(x)
+        #inception(5b)
+        x = self._inception_block(_1x1=352, _3x3r=192, _3x3=320, _d3x3r=192, _d3x3=224, _pool=128)(x)
 
         x_shape = K.int_shape(x)
-
-        x = AveragePooling2D(pool_size = (x_shape[self.row_axis], x_shape[self.col_axis]), strides = (1,1))(x)
-        x = Dense(units = 2048, kernel_initializer=self.initializer)(x)
-        x = BatchNormalization(axis = self.channel_axis)(x)
+        x = AveragePooling2D(pool_size = (x_shape[self.row_axis], x_shape[self.col_axis]), strides=(1,1))(x)
+        x = Flatten()(x)
+        x = Dropout(0.4)(x)
+        x = Dense(units = 1000, kernel_initializer = self.initializer)(x)
+        x = BatchNormalization(axis = 1)(x)
         x = Activation("relu")(x)
+        output_x = Dense(units = self.output_units, activation = "softmax", kernel_initializer=self.initializer, name = "main_output")(x)
 
-        output_x = Dense(units = self.output_units, activation = "softmax", kernel_initializer=self.initializer)
+        inceptionv2_model = Model(inputs = [input_x], outputs = [auxiliary1, auxiliary2, output_x])
 
-        inceptionv2_model = Model(inputs = [input_x], outputs = [output_x,auxiliary])
         return inceptionv2_model
+
+
+inception_builder = Inceptionv2_builder()
+model = inception_builder.build_inception()
+model.summary()
